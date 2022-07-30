33f15056763ffe0f5095eeef90afffbef9ca0a72
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2022-07-29 21:10:52 -0500

    Add files via upload

---
+++ xBACON.R
@@ -0,0 +1,6 @@
+library(readr)
+statcast18 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast18.csv")
+statcast19 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast19.csv")
+statcast20 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast20.csv")
+statcast21 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast21.csv")
+statcast22 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast22.csv")
